81026b78e73839a1624cf5d4b3fe1438a588ea65
darkphoenix2208 <darkphoenix.2208@gmail.com>
2026-05-06 22:19:58 +0530

    ✅ Solved: Rotating the Box (leetcode)

---
+++ rotating-the-box.cpp
@@ -0,0 +1,40 @@
+// Problem  : Rotating the Box
+// Difficulty: Medium
+// Tags     : Array, Two Pointers, Matrix
+// URL      : https://leetcode.com/problems/rotating-the-box/
+// Solved on: 2026-05-08 21:46
+// ──────────────────────────────────────────────────
+
+class Solution {
+public:
+    static vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+        const int r=box.size(), c=box[0].size();
+        vector<vector<char>> rotate(c, vector<char>(r, '.'));
+        for(int i=0; i<r; i++){
+            int bottom=c-1;
+            for(int j=c-1; j>=0; j--){
+                if (box[i][j]=='#'){
+                    rotate[bottom][r-1-i]='#';
+                    bottom--;
+                }
+                else if (box[i][j]=='*'){
+                    rotate[j][r-1-i]='*';
+                    bottom=j-1;
+                }
+            }
+        }
+        return rotate;
+    }
+};
+
+
+
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+// Auto-commit update
