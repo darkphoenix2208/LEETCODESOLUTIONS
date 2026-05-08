896467206477d724dee3b45377ac73b0b701eb7b
darkphoenix2208 <darkphoenix.2208@gmail.com>
2026-05-08 22:10:55 +0530

    ✅ Solved: sudoku-solver (leetcode)

---
+++ sudoku-solver.cpp
@@ -0,0 +1,47 @@
+// Problem  : Sudoku Solver
+// Difficulty: Hard
+// Tags     : Array, Hash Table, Backtracking, Matrix
+// URL      : https://leetcode.com/problems/sudoku-solver/
+// Solved on: 2026-04-09 00:17
+// ──────────────────────────────────────────────────
+
+class Solution {
+public:
+   bool isValid(vector < vector < char >> & board, int row, int col, char c) {
+  for (int i = 0; i < 9; i++) {
+    if (board[i][col] == c)
+      return false;
+
+    if (board[row][i] == c)
+      return false;
+
+    if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
+      return false;
+  }
+  return true;
+}
+
+bool solveSudoku(vector < vector < char >> & board) {
+  for (int i = 0; i < board.size(); i++) {
+    for (int j = 0; j < board[0].size(); j++) {
+      if (board[i][j] == '.') {
+        for (char c = '1'; c <= '9'; c++) {
+          if (isValid(board, i, j, c)) {
+            board[i][j] = c;
+
+            if (solveSudoku(board))
+              return true;
+            else
+              board[i][j] = '.';
+          }
+        }
+
+        return false;
+      }
+    }
+  }
+  return true;
+}
+};
+
+// Auto-commit update
