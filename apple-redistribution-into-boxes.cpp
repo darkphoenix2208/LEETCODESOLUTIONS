228dc8243b6e640e6105922d65d2b9da980939f5
darkphoenix2208 <darkphoenix.2208@gmail.com>
2026-05-08 22:08:36 +0530

    ✅ Solved: apple-redistribution-into-boxes (leetcode)

---
+++ apple-redistribution-into-boxes.cpp
@@ -0,0 +1,33 @@
+// Problem  : Apple Redistribution into Boxes
+// Difficulty: Easy
+// Tags     : Array, Greedy, Sorting
+// URL      : https://leetcode.com/problems/apple-redistribution-into-boxes/
+// Solved on: 2026-04-09 00:01
+// ──────────────────────────────────────────────────
+
+class Solution {
+public:
+    int minimumBoxes(vector<int>& apple, vector<int>& cap) {
+        int sum = accumulate(apple.begin(), apple.end(), 0);
+        uint8_t fq[51] = {};
+        int high = 0, low = 51;
+        for (auto& c : cap) {
+            fq[c]++;
+            high = max(high, c);
+            low = min(low, c);
+        }
+
+        int res = 0;
+        for (int i = high; i >= low && sum > 0; i--) {
+            while (fq[i]-- > 0 && sum > 0) {
+                sum -= i;
+                res++;
+            }
+        }
+
+        return res;
+    }
+};
+
+
+// Auto-commit update
