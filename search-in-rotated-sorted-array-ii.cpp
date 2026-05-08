6dd2969a4bf8af8a47c1d126b139ed05b5de00a9
darkphoenix2208 <darkphoenix.2208@gmail.com>
2026-05-08 22:10:41 +0530

    ✅ Solved: search-in-rotated-sorted-array-ii (leetcode)

---
+++ search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,46 @@
+// Problem  : Search in Rotated Sorted Array II
+// Difficulty: Medium
+// Tags     : Array, Binary Search
+// URL      : https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+// Solved on: 2026-04-09 00:19
+// ──────────────────────────────────────────────────
+
+class Solution {
+public:
+    bool search(vector<int>& arr, int target) {
+        int low = 0, high = arr.size() - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == target)
+                return true;
+
+            if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
+                low++;
+                high--;
+            }
+
+            else if (arr[low] <= arr[mid]) {
+                if (arr[low] <= target && target < arr[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+
+            else {
+                if (arr[mid] < target && target <= arr[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+};
+
+
+// Auto-commit update
