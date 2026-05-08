8e66c069ce1dc67926163e10ffe45e7daf9b94fb
darkphoenix2208 <darkphoenix.2208@gmail.com>
2026-05-08 22:08:54 +0530

    ✅ Solved: count-covered-buildings (leetcode)

---
+++ count-covered-buildings.cpp
@@ -0,0 +1,35 @@
+// Problem  : Count Covered Buildings
+// Difficulty: Medium
+// Tags     : Array, Hash Table, Sorting
+// URL      : https://leetcode.com/problems/count-covered-buildings/
+// Solved on: 2026-04-09 00:02
+// ──────────────────────────────────────────────────
+
+class Solution {
+public:
+    int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
+        vector<int> minx(n + 1, n + 1);
+        vector<int> maxx(n + 1, 0);
+        vector<int> miny(n + 1, n + 1);
+        vector<int> maxy(n + 1, 0);
+        for(auto b : buildings){
+            int x = b[0];
+            int y = b[1];
+            minx[y] = min(minx[y], x);
+            maxx[y] = max(maxx[y], x);
+            miny[x] = min(miny[x], y);
+            maxy[x] = max(maxy[x], y);
+        }
+        int ans = 0;
+        for(auto b : buildings){
+            int x = b[0];
+            int y = b[1];
+            if(minx[y] < x && x < maxx[y] && miny[x] < y && y < maxy[x]){
+                ans += 1;
+            }
+        }
+        return ans;
+    }
+};
+
+// Auto-commit update
