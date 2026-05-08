9b27c7499ac7d93d69a67ca0ba37d1904363c90f
darkphoenix2208 <darkphoenix.2208@gmail.com>
2026-05-08 22:09:44 +0530

    ✅ Solved: maximize-the-number-of-partitions-after-operations (leetcode)

---
+++ maximize-the-number-of-partitions-after-operations.cpp
@@ -0,0 +1,69 @@
+// Problem  : Maximize the Number of Partitions After Operations
+// Difficulty: Hard
+// Tags     : String, Dynamic Programming, Bit Manipulation, Bitmask
+// URL      : https://leetcode.com/problems/maximize-the-number-of-partitions-after-operations/
+// Solved on: 2026-04-09 00:07
+// ──────────────────────────────────────────────────
+
+class Solution {
+public:
+    int maxPartitionsAfterOperations(string s, int k) {
+        int n = s.length();
+        vector<vector<int>> left(n, vector<int>(3)), right(n, vector<int>(3));
+        int num = 0, mask = 0, count = 0;
+        for (int i = 0; i < n - 1; i++) {
+            int binary = 1 << (s[i] - 'a');
+            if (!(mask & binary)) {
+                count++;
+                if (count <= k) {
+                    mask |= binary;
+                } else {
+                    num++;
+                    mask = binary;
+                    count = 1;
+                }
+            }
+            left[i + 1][0] = num;
+            left[i + 1][1] = mask;
+            left[i + 1][2] = count;
+        }
+
+        num = 0, mask = 0, count = 0;
+        for (int i = n - 1; i > 0; i--) {
+            int binary = 1 << (s[i] - 'a');
+            if (!(mask & binary)) {
+                count++;
+                if (count <= k) {
+                    mask |= binary;
+                } else {
+                    num++;
+                    mask = binary;
+                    count = 1;
+                }
+            }
+            right[i - 1][0] = num;
+            right[i - 1][1] = mask;
+            right[i - 1][2] = count;
+        }
+
+        int Max = 0;
+        for (int i = 0; i < n; i++) {
+            int seg = left[i][0] + right[i][0] + 2;
+            int totMask = left[i][1] | right[i][1];
+            int totCount = 0;
+            while (totMask) {
+                totMask = totMask & (totMask - 1);
+                totCount++;
+            }
+            if (left[i][2] == k && right[i][2] == k && totCount < 26) {
+                seg++;
+            } else if (min(totCount + 1, 26) <= k) {
+                seg--;
+            }
+            Max = max(Max, seg);
+        }
+        return Max;
+    }
+};
+
+// Auto-commit update
