8d8e141b27d37acae3bc3f0d46bf6964e944ecce
darkphoenix2208 <darkphoenix.2208@gmail.com>
2026-05-08 22:09:50 +0530

    ✅ Solved: maximum-number-of-k-divisible-components (leetcode)

---
+++ maximum-number-of-k-divisible-components.cpp
@@ -0,0 +1,47 @@
+// Problem  : Maximum Number of K-Divisible Components
+// Difficulty: Hard
+// Tags     : Tree, Depth-First Search
+// URL      : https://leetcode.com/problems/maximum-number-of-k-divisible-components/
+// Solved on: 2026-04-09 00:03
+// ──────────────────────────────────────────────────
+
+class Solution {
+public:
+    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
+        vector<vector<int> > G(n);
+        for(auto edge : edges){
+            G[edge[0]].push_back(edge[1]);
+            G[edge[1]].push_back(edge[0]);
+        }
+        stack <int> S;
+        vector<int> parent(n);
+        
+        auto DFS = [&](int u, int p, auto &&DFS) -> void{
+            parent[u] = p;
+            S.push(u);
+            for(auto v : G[u]){
+                if(v == p) continue;
+                DFS(v, u, DFS);
+            }
+        };
+        DFS(0, -1, DFS);
+        vector<long long> sum(n);
+        for(int i = 0; i < n; i ++){
+            sum[i] = values[i];
+        }
+        int ans = 0;
+        while(!S.empty()){
+            int u = S.top();
+            S.pop();
+            if(sum[u] % k == 0){
+                ans += 1;
+            }
+            else{
+                sum[parent[u]] += sum[u];
+            }
+        }
+        return ans;
+    }
+};
+
+// Auto-commit update
